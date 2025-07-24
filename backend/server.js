5d4d7a112cd52ea867ae6b887bf1c068caefb38b
tarunj292 <tarunj292@gmail.com>
2025-07-24 13:47:54 +0530

    server.js ready

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5002;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
