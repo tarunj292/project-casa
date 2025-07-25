dbd1dcaa893098d27567f564e5a6a3103e288187
Jay Lulia <jaylulia@Jays-MacBook-Air.local>
2025-07-25 11:40:33 +0530

    fixed minor errors while running postman

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5002;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`🚀Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
 app.use('/api/products', productRoutes);
