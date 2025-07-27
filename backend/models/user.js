8a659cf0c879fbe1c95228e62f3dccd531104b19
Vinyas Hegde <vinyasvasanthegde7@gmail.com>
2025-07-27 09:32:03 -0700

    Made additional changes after PR creation

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, sparse: true },
-  phone_number: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
   oauth_provider: { type: String },
   oauth_id: { type: String },
   display_name: { type: String },
