ba579edf09f0b548cae5fd18efaffaa2c953f293
Teesha-M <malkaniteesha@gmail.com>
2025-07-28 09:22:42 +0530

    Brands Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,8 @@ import WishlistPage from './pages/WishlistPage';
 import CheckoutPage from './pages/CheckoutPage';
 import OrderSuccessPage from './pages/OrderSuccessPage';
 import SwipeProductsPage from './pages/SwipeProductsPage';
+import AllBrandsPage from './pages/AllBrandsPage';
+
 
 function App() {
   return (
@@ -19,20 +21,25 @@ function App() {
       <Router>
         <div className="min-h-screen bg-gray-900 text-white mobile-scroll">
           <Routes>
-            <Route path="/" element={<NavShell />}>
-              <Route index element={<HomePage />} />
-              <Route path="collection" element={<CollectionPage />} />
-              <Route path="trends" element={<TrendsPage />} />
-              <Route path="bag" element={<BagPage />} />
-              <Route path="swipe" element={<SwipeProductsPage />} />
-            </Route>
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/wishlist" element={<WishlistPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/order-success" element={<OrderSuccessPage />} />
-          </Routes>
+  <Route path="/" element={<NavShell />}>
+    <Route index element={<HomePage />} />
+    <Route path="collection" element={<CollectionPage />} />
+    <Route path="trends" element={<TrendsPage />} />
+    <Route path="bag" element={<BagPage />} />
+    <Route path="swipe" element={<SwipeProductsPage />} />
+  </Route>
+  
+  {/* ✅ Add new route here */}
+  <Route path="/brands" element={<AllBrandsPage />} />
+
+  <Route path="/product/:id" element={<ProductPage />} />
+  <Route path="/profile" element={<ProfilePage />} />
+  <Route path="/search" element={<SearchPage />} />
+  <Route path="/wishlist" element={<WishlistPage />} />
+  <Route path="/checkout" element={<CheckoutPage />} />
+  <Route path="/order-success" element={<OrderSuccessPage />} />
+</Routes>
+
         </div>
       </Router>
     </div>
